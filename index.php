71373ae62ffe97a5391dc88c430ed6fde3de00eb
Phi Hao <haotppc06836@fpt.edu.vn>
2023-11-05 21:34:35 +0700

    upload giao dien home

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
     require "global.php";
-    require " pdo.php";
+    // require " pdo.php";
     if(exist_param("login")){
 
     }else {
         $view_name = "site/view/home.php";
     }
+    
     require "layout.php";
     ?>
--- layout.php
+++ layout.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="<?=$public_URL?>/css/bootstrap_min.css" rel="stylesheet" >
-	<link href="<?=$public_URL?>/css/font-awesome_min.css" rel="stylesheet" >
-	<link href="<?=$public_URL?>/css/global.css" rel="stylesheet">
-	<link href="<?=$public_URL?>/css/index.css" rel="stylesheet">
+    <link href="public/css/bootstrap_min.css" rel="stylesheet" >
+	<link href="public/css/font-awesome_min.css" rel="stylesheet" >
+	<link href="public/css/global.css" rel="stylesheet">
+	<link href="public/css/index.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
-	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="public/js/bootstrap_bundle_min.js"></script>
 </head>
 
 <body>
