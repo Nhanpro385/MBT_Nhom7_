3bfdcf572a0cb74d0b65ddc1b3b948b867bcfb77
Phi Hao <haotppc06836@fpt.edu.vn>
2023-11-05 20:47:19 +0700

    layouy

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php 
+    require "global.php";
+    require " pdo.php";
+    if(exist_param("login")){
 
-
-?>
+    }else {
+        $view_name = "site/view/home.php";
+    }
+    require "layout.php";
+    ?>
--- layout.php
+++ layout.php
@@ -5,10 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="<?=$public_URL?>/css/bootstrap_min.css" rel="stylesheet" >
+	<link href="<?=$public_URL?>/css/font-awesome_min.css" rel="stylesheet" >
+	<link href="<?=$public_URL?>/css/global.css" rel="stylesheet">
+	<link href="<?=$public_URL?>/css/index.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
+	<script src="js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-
+    <?php 
+        require $view_name;
+    ?>
 </body>
 
 </html>
