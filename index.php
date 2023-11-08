72da612eec78d582d6d740b41415f82f9fbd005f
Phi Hao <haotppc06836@fpt.edu.vn>
2023-11-08 23:09:16 +0700

    11/8 update

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<?php 
-    require "global.php";
-    // require " pdo.php";
-    if(exist_param("login")){
-        $view_name = "site/view/login.php";
-    }else if(exist_param("register")){
-        $view_name = "site/view/register.php";
-    }else {
-        $view_name = "site/view/home.php";
-    }
-    
-    require "layout.php";
-    ?>
+<?php
+require "global.php";
+// require " pdo.php";
+if (exist_param("login")) {
+    $view_name = "site/view/login.php";
+} else if (exist_param("register")) {
+    $view_name = "site/view/register.php";
+} else if (exist_param("seat-show")) {
+    $view_name = "site/view/seat-show.php";
+} else {
+    $view_name = "site/view/home.php";
+}
+
+require "layout.php";
