7ef11e308fec4c49769fc9015112baa7331f13c6
Nhanpro385 <nhanpro385@gmail.com>
2023-11-11 22:47:51 +0700

    fix booking and add movie_genres

--- dao/booking.php
+++ dao/booking.php
@@ -1,13 +1,18 @@
-<?php 
-function insert_booking_history($customer_id,$booking_time,$total_price,$status) {
-    $sql="INSERT INTO booking_history(customer_id,booking_time,total_price,status) VALUES(?,?,?,?)";
-    pdo_execute($sql);
+<?php
+
+function insert_booking_history($customer_id, $booking_time, $total_price, $status) {
+    $sql = "INSERT INTO booking_history(customer_id, booking_time, total_price, status) VALUES(?, ?, ?, ?)";
+    pdo_execute($sql, $customer_id, $booking_time, $total_price, $status);
 }
 
-function select_booking_history($customer_id){
-    $sql=" SELECT * FROM booking_history WHERE customer_id= ?";
-    return pdo_query_one($sql);
+function select_booking_history($customer_id) {
+    $sql = "SELECT * FROM booking_history WHERE customer_id = ?";
+    return pdo_query_one($sql, $customer_id);
 }
 
+function select_booking_history_by_id($booking_id) {
+    $sql = "SELECT * FROM booking_history WHERE booking_id = ?";
+    return pdo_query_one($sql, $booking_id);
+}
 
 ?>
