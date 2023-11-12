3331dc62536ceb32950bf52f9159f4d99ac69391
Nhanpro385 <nhanpro385@gmail.com>
2023-11-13 01:40:42 +0700

    add login page

--- admin/index.php
+++ admin/index.php
@@ -4,11 +4,11 @@ require "../global.php";
 if (exist_param("login")) {
     $view_name = "admin/view/login.php";
 } else if (exist_param("register")) {
-    $view_name = "admin/view/register.php";
+    $view_name = "admin/view/login.php";
 } else if (exist_param("seat-show")) {
-    $view_name = "admin/view/seat-show.php";
+    $view_name = "admin/view/login.php";
 } else {
-    $view_name = "admin/view/home.php";
+    $view_name = "admin/view/login.php";
 }
 
 require "layout.php";
